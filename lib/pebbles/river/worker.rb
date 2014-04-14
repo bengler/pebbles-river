969107a587cf928c4d3b579537bb33a2f404310f
Alexander Staubo <alex@bengler.no>
2014-04-13 23:43:39 -0400

    Managed acking flag, and very careful exception handling.

--- lib/pebbles/river/worker.rb
+++ lib/pebbles/river/worker.rb
@@ -21,6 +21,10 @@ module Pebbles
       #   due to an exception.
       # * `on_connection_error`: If provided, call on recovered connection errors.
       #   Uses `on_exception` if not implemented.
+      # * `managed_acking`: If true, ack/nack handling is automatic; every message
+      #   is automatically acked unless the handler returns false or the handler
+      #   raises an exception, in which case it's nacked. If false, the handler
+      #   must do the ack/nacking. Defaults to true.
       #
       # The handler must implement `call(payload, extra)`, where the payload is
       # the message payload, and the extra argument contains message metadata as
@@ -28,11 +32,18 @@ module Pebbles
       # be nacked. Otherwise, the message with be acked.
       #
       def initialize(handler, options = {})
-        options.assert_valid_keys(:queue, :on_exception, :on_connection_error)
+        options.assert_valid_keys(
+          :queue,
+          :on_exception,
+          :on_connection_error,
+          :managed_acking)
+
         unless handler.respond_to?(:call)
           raise ArgumentError.new('Handler must implement #call protocool')
         end
+
         @queue_options = (options[:queue] || {}).freeze
+        @managed_acking = !!options.fetch(:managed_acking, true)
         @on_exception = options[:on_exception] || ->(e) { }
         @on_connection_error = options[:on_connection_error] || @on_exception
         @handler = handler
@@ -42,26 +53,29 @@ module Pebbles
 
       # Runs the handler once.
       def run_once
-        now = Time.now
-
-        if @next_event_time > now
-          sleep(@next_event_time - now)
+        with_exceptions do
           now = Time.now
-        end
 
-        if should_run?
-          if process_next
-            @next_event_time = now
-          else
-            if @handler.respond_to?(:on_idle)
-              @handler.on_idle
-            end
-            @next_event_time = now + 1
+          if @next_event_time > now
+            sleep(@next_event_time - now)
+            now = Time.now
           end
-        else
-          @next_event_time = now + 5
-        end
 
+          if should_run?
+            if process_next
+              @next_event_time = now
+            else
+              if @handler.respond_to?(:on_idle)
+                with_exceptions do
+                  @handler.on_idle
+                end
+              end
+              @next_event_time = now + 1
+            end
+          else
+            @next_event_time = now + 5
+          end
+        end
         nil
       end
 
@@ -99,63 +113,79 @@ module Pebbles
         end
 
         def process_next
-          queue.pop(auto_ack: false, ack: true) do |raw_message|
-            if raw_message[:payload] != :queue_empty
-              process_message(raw_message)
-              return true
-            else
-              return false
+          with_exceptions do
+            with_connection_error_handling do
+              queue.pop(auto_ack: false, ack: true) do |raw_message|
+                if raw_message[:payload] != :queue_empty
+                  process_message(raw_message)
+                  return true
+                else
+                  return false
+                end
+              end
             end
-          end
-        rescue *CONNECTION_EXCEPTIONS => exception
-          if @queue
-            @queue.close rescue nil
-            @queue = nil
-          end
-
-          @river.disconnect
-
-          begin
-            @on_connection_error.call(exception)
-          rescue
-            # Ignore
-          end
-        rescue => exception
-          begin
-            @on_exception.call(exception)
-          rescue
-            # Ignore
           end
         end
 
         def process_message(raw_message)
           begin
             message = Message.new(raw_message, queue)
-          rescue => e
-            begin
+          rescue => exception
+            ignore_exceptions do
               queue.nack(delivery_tag: message[:delivery_details][:delivery_tag])
-            rescue
-              # Ignore
             end
-            raise
+            raise exception
           else
             begin
               result = @handler.call(message)
             rescue *CONNECTION_EXCEPTIONS
               raise
             rescue => exception
-              begin
-                message.nack
-              rescue
-                # Ignore
+              if @managed_acking
+                ignore_exceptions do
+                  message.nack
+                end
               end
-              raise
+              raise exception
             else
-              if result != false
+              if result != false and @managed_acking
                 message.ack
               end
             end
           end
+        end
+
+        def with_connection_error_handling(&block)
+          yield
+        rescue *CONNECTION_EXCEPTIONS => exception
+          if @queue
+            ignore_exceptions do
+              @queue.close
+            end
+            @queue = nil
+          end
+
+          @river.disconnect
+
+          ignore_exceptions do
+            @on_connection_error.call(exception)
+          end
+        end
+
+        def with_exceptions(&block)
+          begin
+            yield
+          rescue => exception
+            ignore_exceptions do
+              @on_exception.call(exception)
+            end
+          end
+        end
+
+        def ignore_exceptions(&block)
+          yield
+        rescue
+          # Ignore
         end
 
         CONNECTION_EXCEPTIONS = [
